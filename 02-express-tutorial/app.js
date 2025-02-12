03ace5710fb16c9b890dd65b9d889d47e55ba35f
KeshavGuptaRksh <gupta.keshav91@gmail.com>
2025-02-12 08:51:48 +0530

    Final Version
    Complete Node tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+let { people } = require("./data");
+
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide Credentials");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000...");
+});
